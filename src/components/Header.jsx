0da414e736f2786aa3d52ddb1b29354e976ca113
Rohit Bhojak <rohitbhojak99@gmail.com>
2025-10-13 18:24:03 +0530

    Add dark styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ import Logo from "./svg/Logo.jsx";
 import Github from "./svg/Github.jsx";
 export default function Header() {
   return (
-    <header className="w-full flex justify-between items-center p-4">
-      <div className="flex gap-2 items-center text-purple-900">
+    <header className="w-full bg-gray-50 dark:bg-gray-800 flex justify-between items-center p-4 shadow-lg">
+      <div className="flex gap-2 items-center text-purple-800 dark:text-purple-500">
         <Logo width={60} height={60}></Logo>
         <h1 className="text-2xl font-bold">CVmaker</h1>
       </div>
       <a href="https://github.com/RohitBhojak/CVmaker">
-        <Github className="fill-purple-900"></Github>
+        <Github className="fill-purple-800 dark:fill-purple-500"></Github>
       </a>
     </header>
   );
