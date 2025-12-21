0e829f007df0ccae8f652ea67c9a8ba1f3b47984
Rohit Bhojak <rohitbhojak99@gmail.com>
2025-12-21 12:21:56 +0530

    Add pencil icon on edit button

--- src/components/UserDetails/common/SubmitEditButton.jsx
+++ src/components/UserDetails/common/SubmitEditButton.jsx
@@ -1,11 +1,15 @@
 import Button from "../../common/Button";
+import { PencilIcon } from "lucide-react";
 export default function SubmitEditButton({ isEdit, onSubmit, onEdit }) {
   return (
     <>
       {isEdit ? (
         <Button onClick={onSubmit}>Submit</Button>
       ) : (
-        <Button onClick={onEdit}>Edit</Button>
+        <Button onClick={onEdit}>
+          <PencilIcon />
+          Edit
+        </Button>
       )}
     </>
   );
