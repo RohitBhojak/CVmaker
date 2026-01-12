b78f018f3eb63b9c8e90624235438d141e1d7635
Rohit Bhojak <rohitbhojak99@gmail.com>
2026-01-12 09:14:55 +0530

    Reduce header logo size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,15 @@ export default function Header() {
   return (
     <header className="w-full bg-gray-50 dark:bg-gray-800 flex justify-between items-center p-4 shadow-mg">
       <div className="flex gap-2 items-center text-purple-700 dark:text-purple-500">
-        <Logo width={50} height={50}></Logo>
+        <Logo width={44} height={44}></Logo>
         <h1 className="text-4xl font-bold">CVmaker</h1>
       </div>
       <a href="https://github.com/RohitBhojak/CVmaker">
-        <Github className="fill-purple-700 dark:fill-purple-500 hover:fill-purple-500 dark:hover:fill-purple-700"></Github>
+        <Github
+          width={44}
+          height={44}
+          className="fill-purple-700 dark:fill-purple-500 hover:fill-purple-500 dark:hover:fill-purple-700"
+        ></Github>
       </a>
     </header>
   );
